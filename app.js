f85876dff1a0b45b1e44c0dfe9237d5691b3aba9
Ömer Can <67295748+omercandemir@users.noreply.github.com>
2022-01-01 16:24:00 +0300

    mail settings finished and project finished.

--- app.js
+++ app.js
@@ -1,22 +1,74 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
-const nodeMailer = require("nodemailer");
+const { engine } = require('express-handlebars');
+const path = require("path");
+const nodemailer = require("nodemailer");
 
 const port = 3000;
 
 const app = express();
 
-// view Engine Setup
-app.engine('handlebars', exphbs());
+// view engine setup
+// app.engine('handlebars', exphbs());
+app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+app.set('views', './views');
 
-// Body parser middleware
+// body parser middleware
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
+// public folder
+
+app.use("/public", express.static(path.join(__dirname + '/public')));
+
 app.get('/', (req, res) => {
-    res.send('Welcome');
+    res.render('contact');
 });
+
+app.post('/send', (req, res) => {
+    const output = `
+        <h3>Contact Information</h3>
+        <ul>
+            <li>Name: ${req.body.name}</li>
+            <li>Email: ${req.body.email}</li>
+            <li>Phone: ${req.body.phone}</li>
+            <li>Subject: ${req.body.subject}</li>
+            <li>Message: ${req.body.message}</li>
+        </ul>
+    `;
+    sendMail(req.body.subject, output);
+    res.render('contact', {msg: '<div class="alert alert-success">Email sent successfully!</div>'})
+});
+
+async function sendMail(subject, message)
+{
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "e415496fbe3dde",
+          pass: "2406773511f1fa"
+        }
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Nodemailer Contact Test 👻" <omer@ayicik.com>', // sender address
+      to: "inkilap28@gmail.com", // list of receivers
+      subject: subject, // Subject line
+      text: "New Contact Request", // plain text body
+      html: message, // html body
+    });
+
+    transporter.sendMail(info, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("Message sent: %s", info.messageId);
+        console.log("Preview URL: %s", info.nodemailer.getTestMessageUrl(info));
+    });
+}
 
 app.listen(port, () => console.log("Server is running..."));
